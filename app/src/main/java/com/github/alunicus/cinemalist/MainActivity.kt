813993ebdae215a1f30e6858a5522a4a98e41780
alunicus <alunicus@gmail.com>
2019-10-23 00:49:13 +0300

    Get simple search result from the server

--- app/src/main/java/com/github/alunicus/cinemalist/MainActivity.kt
+++ app/src/main/java/com/github/alunicus/cinemalist/MainActivity.kt
@@ -1,12 +1,35 @@
 package com.github.alunicus.cinemalist
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.google.gson.FieldNamingPolicy
+import com.google.gson.GsonBuilder
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val gson = GsonBuilder()
+            .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
+            .create()
+
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://api.themoviedb.org/")
+            .addConverterFactory(GsonConverterFactory.create(gson))
+            .build()
+
+        val service = retrofit.create(SearchApi::class.java)
+
+        GlobalScope.launch {
+            service.getSearchResult("Rocky", "")
+                .results.forEach { Log.d("TAG", it.title) }
+        }
     }
 }
--- app/src/main/java/com/github/alunicus/cinemalist/SearchApi.kt
+++ app/src/main/java/com/github/alunicus/cinemalist/SearchApi.kt
@@ -2,9 +2,9 @@ package com.github.alunicus.cinemalist
 
 import com.github.alunicus.cinemalist.data.SearchResult
 import retrofit2.http.GET
-import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface SearchApi {
     @GET("3/search/movie")
-    suspend fun listRepos(@Path("query") query: String, @Path("api_key") apiKey: String): List<SearchResult>
+    suspend fun getSearchResult(@Query("query") query: String, @Query("api_key") apiKey: String): SearchResult
 }
