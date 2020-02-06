3fbb13625a738602c9cd7bf02583872a1834b051
alunicus <alunicus@gmail.com>
2020-02-06 21:28:49 +0200

    Add resource loader for test json responses

---
+++ app/src/test/java/com/github/alunicus/cinemalist/ResourceLoader.kt
@@ -0,0 +1,20 @@
+package com.github.alunicus.cinemalist
+
+import com.google.gson.FieldNamingPolicy
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import java.io.File
+
+class ResourceLoader {
+    val gson: Gson = GsonBuilder()
+        .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
+        .create()
+
+    inline fun <reified T> readFromJson(fileName: String): T {
+        val file = this::class.java.classLoader?.getResource(fileName)!!.file
+
+        val jsonText = File(file).readText(Charsets.UTF_8).trimIndent()
+
+        return gson.fromJson<T>(jsonText, T::class.java)
+    }
+}
