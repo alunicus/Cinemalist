d3ced3402248bb4b49fa9b4158cca4728a654876
alunicus <alunicus@gmail.com>
2020-02-13 23:21:18 +0200

    Add population of movie to the view

--- app/src/main/java/com/github/alunicus/cinemalist/movie/MovieFragment.kt
+++ app/src/main/java/com/github/alunicus/cinemalist/movie/MovieFragment.kt
@@ -5,7 +5,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.github.alunicus.cinemalist.R
+import androidx.lifecycle.Observer
+import com.github.alunicus.cinemalist.data.Movie
+import com.github.alunicus.cinemalist.databinding.MovieFragmentBinding
 import org.koin.android.viewmodel.ext.android.viewModel
 
 class MovieFragment : Fragment() {
@@ -13,13 +15,42 @@ class MovieFragment : Fragment() {
         fun newInstance() = MovieFragment()
     }
 
+    private var _binding: MovieFragmentBinding? = null
+    private val binding get() = _binding!!
+
     private val viewModel: MovieViewModel by viewModel()
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.movie_fragment, container, false)
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = MovieFragmentBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+
+        viewModel.onMovieLoaded().observe(viewLifecycleOwner, Observer {
+            populateView(it)
+        })
+
+        viewModel.loadMovie(458156)
+    }
+
+    private fun populateView(movie: Movie) {
+        binding.apply {
+            movieTitle.text = movie.title
+            movieYear.text = movie.releaseDate
+            movieDuration.text = movie.runtime.toString()
+            movieOverview.text = movie.overview
+        }
+    }
+
+    override fun onDestroyView() {
+        _binding = null
+
+        super.onDestroyView()
     }
 }
--- app/src/main/java/com/github/alunicus/cinemalist/movie/MovieViewModel.kt
+++ app/src/main/java/com/github/alunicus/cinemalist/movie/MovieViewModel.kt
@@ -1,6 +1,20 @@
 package com.github.alunicus.cinemalist.movie
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.github.alunicus.cinemalist.data.Movie
+import kotlinx.coroutines.launch
 
-class MovieViewModel(repository : MovieRepository) : ViewModel() {
+class MovieViewModel(private val repository: MovieRepository) : ViewModel() {
+    private val movieLoaded by lazy { MutableLiveData<Movie>() }
+
+    public fun onMovieLoaded(): LiveData<Movie> = movieLoaded
+
+    public fun loadMovie(movieId: Int) {
+        viewModelScope.launch {
+            movieLoaded.value = repository.getMovieById(movieId)
+        }
+    }
 }
