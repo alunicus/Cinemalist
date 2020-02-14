2a74c6f614393ebb806fa7ed990756d64c215380
alunicus <alunicus@gmail.com>
2020-02-14 22:27:31 +0200

    Add a loading of poster using Coil

--- app/src/main/java/com/github/alunicus/cinemalist/movie/MovieFragment.kt
+++ app/src/main/java/com/github/alunicus/cinemalist/movie/MovieFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.observe
+import coil.api.load
 import com.github.alunicus.cinemalist.data.Movie
 import com.github.alunicus.cinemalist.databinding.MovieFragmentBinding
 import org.koin.android.viewmodel.ext.android.viewModel
@@ -41,6 +42,7 @@ class MovieFragment : Fragment() {
 
     private fun populateView(movie: Movie) {
         binding.apply {
+            moviePoster.load("https://image.tmdb.org/t/p/original/${movie.posterPath}")
             movieTitle.text = movie.title
             movieYear.text = movie.releaseDate
             movieDuration.text = movie.runtime.toString()
