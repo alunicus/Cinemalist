a436db3a2e651b553dcc7908ec3e997233e42d4f
alunicus <alunicus@gmail.com>
2020-03-11 22:54:58 +0200

    Add crossfade effect fot image loading

--- app/src/main/java/com/github/alunicus/cinemalist/extensions/ImageLoadingExtension.kt
+++ app/src/main/java/com/github/alunicus/cinemalist/extensions/ImageLoadingExtension.kt
@@ -6,5 +6,7 @@ import coil.api.load
 const val BASE_URL_POSTER = "https://image.tmdb.org/t/p/original/"
 
 fun ImageView.loadPoster(url: String) {
-    this.load("$BASE_URL_POSTER$url")
+    this.load("$BASE_URL_POSTER$url") {
+        crossfade(true)
+    }
 }
