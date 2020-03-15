1b8c835e83dfedd7bd87a5cb9c835dc95ae71e63
alunicus <alunicus@gmail.com>
2020-03-16 00:22:09 +0200

    Add adapter for movie's cast

---
+++ app/src/main/java/com/github/alunicus/cinemalist/feature/movie/CastAdapter.kt
@@ -0,0 +1,39 @@
+package com.github.alunicus.cinemalist.feature.movie
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.github.alunicus.cinemalist.data.Person
+import com.github.alunicus.cinemalist.databinding.ItemCastBinding
+import com.github.alunicus.cinemalist.extensions.loadCircleImage
+
+class CastAdapter : RecyclerView.Adapter<CastAdapter.ViewHolder>() {
+    private var _binding: ItemCastBinding? = null
+    private val binding get() = _binding!!
+
+    private val items = mutableListOf<Person>()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        _binding = ItemCastBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+
+        return ViewHolder(binding)
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.apply {
+            val person = items[adapterPosition]
+
+            name.text = person.name
+            photo.loadCircleImage(person.photoUrl)
+        }
+    }
+
+    class ViewHolder(binding: ItemCastBinding) : RecyclerView.ViewHolder(binding.root) {
+        val photo = binding.castPhoto
+        val name = binding.castName
+    }
+}
