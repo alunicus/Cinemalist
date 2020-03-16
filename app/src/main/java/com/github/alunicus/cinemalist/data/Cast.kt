b84b0dc0762a4fe6bc188cedb2f22602405d35c2
alunicus <alunicus@gmail.com>
2020-03-16 23:28:54 +0200

    Add Cast model and mapper

---
+++ app/src/main/java/com/github/alunicus/cinemalist/data/Cast.kt
@@ -0,0 +1,9 @@
+package com.github.alunicus.cinemalist.data
+
+data class Cast(
+    val id: Int,
+    val name: String,
+    val character: String,
+    val profilePath: String,
+    val order: Int
+)
--- app/src/main/java/com/github/alunicus/cinemalist/extensions/DtoExtensions.kt
+++ app/src/main/java/com/github/alunicus/cinemalist/extensions/DtoExtensions.kt
@@ -1,11 +1,10 @@
 package com.github.alunicus.cinemalist.extensions
 
+import com.github.alunicus.cinemalist.data.Cast
 import com.github.alunicus.cinemalist.data.Movie
 import com.github.alunicus.cinemalist.data.SearchMovie
 import com.github.alunicus.cinemalist.data.SearchResult
-import com.github.alunicus.cinemalist.data.dto.MovieDto
-import com.github.alunicus.cinemalist.data.dto.SearchMovieDto
-import com.github.alunicus.cinemalist.data.dto.SearchResultsDto
+import com.github.alunicus.cinemalist.data.dto.*
 
 fun MovieDto.toMovie() = Movie(
     id,
@@ -26,6 +25,18 @@ fun MovieDto.toMovie() = Movie(
     video,
     voteAverage,
     voteCount
+)
+
+fun MovieCreditsDto.toCast() = cast.toCastList()
+
+private fun List<CastDto>.toCastList() = asSequence().map { it.toCast() }.toList()
+
+private fun CastDto.toCast() = Cast(
+    id,
+    name,
+    character,
+    profilePath,
+    order
 )
 
 fun SearchResultsDto.toSearchResult() = SearchResult(
