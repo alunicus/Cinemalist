05d223fd898c3d457ea35319fdfbf1ffb59f6f8b
alunicus <alunicus@gmail.com>
2020-03-18 23:53:22 +0200

    Add generic request method to wrap network result

--- app/src/main/java/com/github/alunicus/cinemalist/feature/movie/MovieRepositoryImpl.kt
+++ app/src/main/java/com/github/alunicus/cinemalist/feature/movie/MovieRepositoryImpl.kt
@@ -13,16 +13,16 @@ class MovieRepositoryImpl(network: Network) : MovieRepository {
     private var api: MovieApi = network.createRetrofit(MovieApi::class.java)
 
     override suspend fun getMovieById(id: Int): Result<Movie, Error> {
-        return try {
-            Result.Success(api.getMovieById(id, BuildConfig.API_KEY).toMovie())
-        } catch (e: Exception) {
-            Result.Failure(Error.ServerError)
-        }
+        return request { api.getMovieById(id, BuildConfig.API_KEY).toMovie() }
     }
 
     override suspend fun getMovieCast(id: Int): Result<List<Cast>, Error> {
+        return request { api.getMovieCredits(id, BuildConfig.API_KEY).toCast() }
+    }
+
+    private suspend fun <T> request(call: suspend () -> T): Result<T, Error> {
         return try {
-            Result.Success(api.getMovieCredits(id, BuildConfig.API_KEY).toCast())
+            Result.Success(call.invoke())
         } catch (e: Exception) {
             Result.Failure(Error.ServerError)
         }
