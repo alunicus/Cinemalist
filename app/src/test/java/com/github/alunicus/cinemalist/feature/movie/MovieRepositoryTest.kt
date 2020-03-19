ef8f7b8c3dce4095ae6f1fc2869bf9e3a336ab6a
alunicus <alunicus@gmail.com>
2020-03-19 23:45:27 +0200

    Add MovieRepository empty tests

---
+++ app/src/test/java/com/github/alunicus/cinemalist/feature/movie/MovieRepositoryTest.kt
@@ -0,0 +1,32 @@
+package com.github.alunicus.cinemalist.feature.movie
+
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+
+class MovieRepositoryTest {
+    @Nested
+    inner class GetMovieByIdTest {
+        @Test
+        fun `should return movie by id`() {
+
+        }
+
+        @Test
+        fun `should return server error for a movie`() {
+
+        }
+    }
+
+    @Nested
+    inner class GetMovieCastTest {
+        @Test
+        fun `should return cast by movie id`() {
+
+        }
+
+        @Test
+        fun `should return server error for a movie cast`() {
+
+        }
+    }
+}
