a0b0d3961232cc5a98b803bc11aa058a9442cbcc
alunicus <alunicus@gmail.com>
2020-03-25 21:34:58 +0200

    Use DI for MovieApi in MovieRemoteDataSource

--- app/src/main/java/com/github/alunicus/cinemalist/KoinModules.kt
+++ app/src/main/java/com/github/alunicus/cinemalist/KoinModules.kt
@@ -1,20 +1,35 @@
 package com.github.alunicus.cinemalist
 
-import com.github.alunicus.cinemalist.feature.movie.data.MovieRemoteDataSource
-import com.github.alunicus.cinemalist.feature.movie.data.MovieRemoteDataSourceImpl
-import com.github.alunicus.cinemalist.feature.movie.data.MovieRepository
-import com.github.alunicus.cinemalist.feature.movie.data.MovieRepositoryImpl
+import com.github.alunicus.cinemalist.feature.movie.data.*
 import com.github.alunicus.cinemalist.feature.movie.domain.GetMovieCastUseCase
 import com.github.alunicus.cinemalist.feature.movie.domain.GetMovieUseCase
 import com.github.alunicus.cinemalist.feature.movie.presentation.MovieViewModel
+import com.google.gson.FieldNamingPolicy
+import com.google.gson.GsonBuilder
 import org.koin.android.viewmodel.dsl.viewModel
 import org.koin.dsl.module
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 val modules = module {
-    single<MovieRemoteDataSource> { MovieRemoteDataSourceImpl() }
 }
 
 val movieModule = module {
+    factory {
+        val gson = GsonBuilder()
+            .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
+            .setDateFormat("yyyy-MM-dd")
+            .create()
+
+        Retrofit.Builder()
+            .baseUrl("https://api.themoviedb.org/3/")
+            .addConverterFactory(GsonConverterFactory.create(gson))
+            .build()
+            .create(MovieApi::class.java)
+    }
+
+    single<MovieRemoteDataSource> { MovieRemoteDataSourceImpl(get()) }
+
     single<MovieRepository> { MovieRepositoryImpl(get()) }
 
     viewModel { MovieViewModel(get(), get()) }
--- app/src/main/java/com/github/alunicus/cinemalist/feature/movie/data/MovieRemoteDataSourceImpl.kt
+++ app/src/main/java/com/github/alunicus/cinemalist/feature/movie/data/MovieRemoteDataSourceImpl.kt
@@ -5,26 +5,8 @@ import com.github.alunicus.cinemalist.extensions.toCast
 import com.github.alunicus.cinemalist.extensions.toMovie
 import com.github.alunicus.cinemalist.feature.movie.domain.model.Cast
 import com.github.alunicus.cinemalist.feature.movie.domain.model.Movie
-import com.google.gson.FieldNamingPolicy
-import com.google.gson.GsonBuilder
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 
-class MovieRemoteDataSourceImpl : MovieRemoteDataSource {
-    private val api: MovieApi by lazy {
-        val gson = GsonBuilder()
-            .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
-            .setDateFormat("yyyy-MM-dd")
-            .create()
-
-        val retrofit = Retrofit.Builder()
-            .baseUrl("https://api.themoviedb.org/3/")
-            .addConverterFactory(GsonConverterFactory.create(gson))
-            .build()
-
-        retrofit.create(MovieApi::class.java)
-    }
-
+class MovieRemoteDataSourceImpl(private val api: MovieApi) : MovieRemoteDataSource {
     override suspend fun getMovieById(movieId: Int): Movie {
         return api.getMovieById(movieId, BuildConfig.API_KEY).toMovie()
     }
