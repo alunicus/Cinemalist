1deef004925dcc44ea72289292d1979f622bf19e
alunicus <alunicus@gmail.com>
2020-03-25 22:50:19 +0200

    Fix test cases for MovieRepository

--- app/src/test/java/com/github/alunicus/cinemalist/feature/movie/MovieRepositoryTest.kt
+++ app/src/test/java/com/github/alunicus/cinemalist/feature/movie/MovieRepositoryTest.kt
@@ -1,11 +1,9 @@
 package com.github.alunicus.cinemalist.feature.movie
 
-import com.github.alunicus.cinemalist.ResourceLoader
 import com.github.alunicus.cinemalist.core.Result
 import com.github.alunicus.cinemalist.feature.movie.data.MovieRemoteDataSource
 import com.github.alunicus.cinemalist.feature.movie.data.MovieRepository
 import com.github.alunicus.cinemalist.feature.movie.data.MovieRepositoryImpl
-import com.github.alunicus.cinemalist.feature.movie.domain.model.Movie
 import io.mockk.coEvery
 import io.mockk.mockk
 import kotlinx.coroutines.runBlocking
@@ -15,9 +13,6 @@ import org.junit.jupiter.api.Test
 import org.koin.test.KoinTest
 
 class MovieRepositoryTest : KoinTest {
-
-    private val resourceLoader = ResourceLoader("./feature/movie/")
-
     private val movieRemoteDataSource: MovieRemoteDataSource = mockk()
 
     private val repository: MovieRepository = MovieRepositoryImpl(movieRemoteDataSource)
@@ -28,25 +23,12 @@ class MovieRepositoryTest : KoinTest {
         fun `should return success object if request is fine`() {
             runBlocking {
                 coEvery { movieRemoteDataSource.getMovieById(1) }
-                    .returns(resourceLoader.readFromJson("movie.json"))
+                    .returns(mockk())
 
                 val actualResult = repository.getMovieById(1)
 
                 assertThat(actualResult)
                     .isInstanceOf(Result.Success::class.java)
-            }
-        }
-
-        @Test
-        fun `should return mapped instance of Movie in case of success`() {
-            coEvery { movieRemoteDataSource.getMovieById(1) }
-                .returns(resourceLoader.readFromJson("movie.json"))
-
-            runBlocking {
-                val actualResult = (repository.getMovieById(1) as Result.Success).result
-
-                assertThat(actualResult)
-                    .isInstanceOf(Movie::class.java)
             }
         }
 
@@ -69,27 +51,13 @@ class MovieRepositoryTest : KoinTest {
         @Test
         fun `should return success object if request is fine`() {
             coEvery { movieRemoteDataSource.getMovieCredits(1) }
-                .returns(resourceLoader.readFromJson("credits.json"))
+                .returns(mockk())
 
             runBlocking {
                 val actualResult = repository.getMovieCast(1)
 
                 assertThat(actualResult)
                     .isInstanceOf(Result.Success::class.java)
-            }
-        }
-
-        @Test
-        fun `should return mapped instance of Cast by movie id in case of success`() {
-            coEvery { movieRemoteDataSource.getMovieCredits(1) }
-                .returns(resourceLoader.readFromJson("credits.json"))
-
-            runBlocking {
-                val actualResult = (repository.getMovieCast(1) as Result.Success).result
-
-                assertThat(actualResult)
-                    .isInstanceOf(List::class.java)
-                    .hasSize(7)
             }
         }
 
