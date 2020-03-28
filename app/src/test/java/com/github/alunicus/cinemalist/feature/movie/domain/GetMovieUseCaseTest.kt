472aa885e96f8ad7e4193cfe074ee43fee9155c6
alunicus <alunicus@gmail.com>
2020-03-28 23:23:50 +0200

    Add test cases for GetMovieUseCaseTest

--- app/src/test/java/com/github/alunicus/cinemalist/feature/movie/domain/GetMovieUseCaseTest.kt
+++ app/src/test/java/com/github/alunicus/cinemalist/feature/movie/domain/GetMovieUseCaseTest.kt
@@ -1,9 +1,11 @@
 package com.github.alunicus.cinemalist.feature.movie.domain
 
+import com.github.alunicus.cinemalist.core.Error
+import com.github.alunicus.cinemalist.core.Result.Failure
 import com.github.alunicus.cinemalist.core.Result.Success
 import com.github.alunicus.cinemalist.feature.movie.data.MovieRepository
-import com.github.alunicus.cinemalist.feature.movie.domain.model.Movie
 import io.mockk.coEvery
+import io.mockk.coVerify
 import io.mockk.mockk
 import kotlinx.coroutines.runBlocking
 import org.assertj.core.api.Assertions.assertThat
@@ -15,16 +17,60 @@ class GetMovieUseCaseTest {
 
     private val useCase = GetMovieUseCase(movieRepository)
 
+    private val testMovieId = 458156
+
     @Test
     fun `should return Movie if getting of movie details and cast are successful`() {
-        coEvery { movieRepository.getMovieDetailsById(1) }.returns(Success(mockk()))
-        coEvery { movieRepository.getMovieCast(1) }.returns(Success(listOf(mockk(), mockk())))
+        val testMovieDetails = getTestMovieDetailsWithoutNulls()
+        val testCastList = getTestCastList()
+
+        coEvery { movieRepository.getMovieDetailsById(testMovieId) }
+            .returns(Success(testMovieDetails))
+
+        coEvery { movieRepository.getMovieCast(testMovieId) }.returns(Success(testCastList))
 
         runBlocking {
-            val result = (useCase.getMovie(1) as Success).result
+            val result = (useCase.getMovie(testMovieId) as Success).result
 
-            assertThat(result).isInstanceOf(Movie::class.java)
-            assertThat(result.cast).hasSize(2)
+            assertThat(result.movieDetails).isEqualTo(testMovieDetails)
+            assertThat(result.cast).isEqualTo(testCastList)
         }
+    }
+
+    @Test
+    fun `should return Movie with cast empty list if getting of cast failed`() {
+        val testMovieDetails = getTestMovieDetailsWithoutNulls()
+
+        coEvery { movieRepository.getMovieDetailsById(testMovieId) }
+            .returns(Success(testMovieDetails))
+
+        coEvery { movieRepository.getMovieCast(testMovieId) }.returns(Failure(Error.ServerError))
+
+        runBlocking {
+            val result = (useCase.getMovie(testMovieId) as Success).result
+
+            assertThat(result.movieDetails).isEqualTo(testMovieDetails)
+            assertThat(result.cast).isEmpty()
+        }
+    }
+
+    @Test
+    fun `should return Failure from failed movie details request`() {
+        val testMovieFailure = Failure(Error.ServerError)
+
+        coEvery { movieRepository.getMovieDetailsById(testMovieId) }.returns(testMovieFailure)
+
+        runBlocking {
+            assertThat(useCase.getMovie(testMovieId)).isEqualTo(testMovieFailure)
+        }
+    }
+
+    @Test
+    fun `should not call getMovieCast if movie details failed`() {
+        val testMovieFailure = Failure(Error.ServerError)
+
+        coEvery { movieRepository.getMovieDetailsById(testMovieId) }.returns(testMovieFailure)
+
+        coVerify(exactly = 0) { movieRepository.getMovieCast(testMovieId) }
     }
 }
