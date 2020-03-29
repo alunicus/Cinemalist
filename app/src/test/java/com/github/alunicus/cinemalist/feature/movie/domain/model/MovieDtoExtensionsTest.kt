4a0fb570a1529c0758fbec5781e162add771b676
alunicus <alunicus@gmail.com>
2020-03-29 21:17:07 +0300

    Add test cases for the cast mapper

--- app/src/test/java/com/github/alunicus/cinemalist/feature/movie/domain/model/MovieDtoExtensionsTest.kt
+++ app/src/test/java/com/github/alunicus/cinemalist/feature/movie/domain/model/MovieDtoExtensionsTest.kt
@@ -1,8 +1,11 @@
 package com.github.alunicus.cinemalist.feature.movie.domain.model
 
 import com.github.alunicus.cinemalist.ResourceLoader
+import com.github.alunicus.cinemalist.feature.movie.domain.getTestCastList
+import com.github.alunicus.cinemalist.feature.movie.domain.getTestCastListWithoutNulls
 import com.github.alunicus.cinemalist.feature.movie.domain.getTestMovieDetails
 import com.github.alunicus.cinemalist.feature.movie.domain.getTestMovieDetailsWithoutNulls
+import com.github.alunicus.cinemalist.feature.movie.domain.model.dto.MovieCreditsDto
 import com.github.alunicus.cinemalist.feature.movie.domain.model.dto.MovieDetailsDto
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
@@ -12,15 +15,33 @@ class MovieDtoExtensionsTest {
 
     @Test
     fun `should return Movie from movieDto without null fields`() {
-        val actualResult = resourceLoader.readFromJson<MovieDetailsDto>("movie_dto.json").toMovie()
+        val actualResult = resourceLoader
+            .readFromJson<MovieDetailsDto>("movie_dto.json").toMovie()
 
         assertThat(actualResult).isEqualTo(getTestMovieDetailsWithoutNulls())
     }
 
     @Test
     fun `should return Movie from movieDto with correctly handled null fields`() {
-        val movieDto = resourceLoader.readFromJson<MovieDetailsDto>("movie_dto_with_nulls.json")
+        val actualResult = resourceLoader
+            .readFromJson<MovieDetailsDto>("movie_dto_with_nulls.json").toMovie()
 
-        assertThat(movieDto.toMovie()).isEqualTo(getTestMovieDetails())
+        assertThat(actualResult).isEqualTo(getTestMovieDetails())
+    }
+
+    @Test
+    fun `should return Cast list from movieCreditsDto without null fields`() {
+        val actualResult = resourceLoader
+            .readFromJson<MovieCreditsDto>("movie_credits_dto.json").toCast()
+
+        assertThat(actualResult).isEqualTo(getTestCastListWithoutNulls())
+    }
+
+    @Test
+    fun `should return Cast list from movieCreditsDto with correctly handled null fields`() {
+        val actualResult = resourceLoader
+            .readFromJson<MovieCreditsDto>("movie_credits_dto_with_nulls.json").toCast()
+
+        assertThat(actualResult).isEqualTo(getTestCastList())
     }
 }
