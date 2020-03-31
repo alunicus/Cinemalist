d9782cb8601954d01953390ec5da52c9b72ebded
alunicus <alunicus@gmail.com>
2020-03-31 21:56:47 +0300

    Add error placeholder to the image loader

--- app/src/main/java/com/github/alunicus/cinemalist/extensions/ImageLoadingExtension.kt
+++ app/src/main/java/com/github/alunicus/cinemalist/extensions/ImageLoadingExtension.kt
@@ -5,12 +5,18 @@ import android.widget.ImageView
 import coil.api.load
 import coil.transform.BlurTransformation
 import coil.transform.CircleCropTransformation
+import com.github.alunicus.cinemalist.R
 
 const val BASE_IMAGE_URL = "https://image.tmdb.org/t/p/original/"
 
 fun ImageView.loadImage(url: String) {
+    loadImage(url, R.drawable.movie_placeholder)
+}
+
+fun ImageView.loadImage(url: String, onErrorResId: Int) {
     this.load("$BASE_IMAGE_URL$url") {
         crossfade(true)
+        error(onErrorResId)
     }
 }
 
