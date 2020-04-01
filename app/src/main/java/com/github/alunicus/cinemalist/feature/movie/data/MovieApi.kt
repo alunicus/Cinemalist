d60513ed6c9ab7111e142334642a381add05a328
alunicus <alunicus@gmail.com>
2020-04-01 22:21:29 +0300

    Add popular movie endpoint

--- app/src/main/java/com/github/alunicus/cinemalist/feature/movie/data/MovieApi.kt
+++ app/src/main/java/com/github/alunicus/cinemalist/feature/movie/data/MovieApi.kt
@@ -2,6 +2,7 @@ package com.github.alunicus.cinemalist.feature.movie.data
 
 import com.github.alunicus.cinemalist.feature.movie.domain.model.dto.MovieCreditsDto
 import com.github.alunicus.cinemalist.feature.movie.domain.model.dto.MovieDetailsDto
+import com.github.alunicus.cinemalist.feature.movie.domain.model.dto.PopularMovieDto
 import retrofit2.http.GET
 import retrofit2.http.Path
 
@@ -11,4 +12,7 @@ interface MovieApi {
 
     @GET("movie/{movie_id}/credits")
     suspend fun getMovieCredits(@Path("movie_id") movieId: Int): MovieCreditsDto
+
+    @GET("/movie/popular")
+    suspend fun getPopularMovies(): PopularMovieDto
 }
