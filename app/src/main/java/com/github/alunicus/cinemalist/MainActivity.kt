fd06ce26c0c194a8d3ce2188ac80c86bd647ab49
alunicus <alunicus@gmail.com>
2020-04-17 11:39:05 +0300

    Fix back button in the toolbar using navigation component

--- app/src/main/java/com/github/alunicus/cinemalist/MainActivity.kt
+++ app/src/main/java/com/github/alunicus/cinemalist/MainActivity.kt
@@ -2,18 +2,28 @@ package com.github.alunicus.cinemalist
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.navigation.ui.NavigationUI
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        navController = Navigation.findNavController(this, R.id.activity_main_host_fragment)
+
         NavigationUI.setupWithNavController(
             findViewById<BottomNavigationView>(R.id.activity_main_bottom_navigation),
-            Navigation.findNavController(this, R.id.activity_main_host_fragment)
+            navController
         )
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp()
     }
 }
